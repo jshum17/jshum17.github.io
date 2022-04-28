29d86ad87e9956c7204a59614892057adb2324e2
Justin Shum <91106388+jshum17@users.noreply.github.com>
2022-04-28 00:05:50 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+
+   //Navbar scroll down/change colour
+
+   $(window).scroll(function(){
+      if(this.scrollY > 20){
+         $('.navbar').addClass("sticky");
+      }else{
+         $('.navbar').removeClass("sticky");
+      }
+   });
+
+   //Home down btn smooth scrolling
+   $("a[href^=\\#welcome]").on('click', function(event) {
+    
+      if (this.hash !== "") {
+
+        event.preventDefault();
+
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 700, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+   });
+
+   //Toggle Menu
+
+   $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.navbar').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+      $("html").toggleClass("noscroll");
+      $('body').toggleClass("active");
+      $('.nav-outline-top').toggleClass("active");
+   });
+
+   //Typing animation
+
+   var typed = new Typed(".typing",{
+      strings:["Page","Website"],
+      typeSpeed:120,
+      backSpeed:80,
+      loop:true
+   });
+
+   //About Pop Up
+
+   $(window).scroll( function(){
+    
+      /* Check the location of each desired element */
+      $('about-desc').each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+            $(this).animate({'opacity':'1'},600).addClass('animate__animated animate__fadeInUp');    
+         } 
+      }); 
+   });
+
+   //Gallery Lightbox
+   
+   $('.gallery-img').click(function(){
+      $('.lightbox').addClass("active");
+      const img = $(this).clone();
+      $('.lightbox').append(img);
+      $("html").addClass("noscroll");
+   });
+
+   $('.lightbox').click(function(event){
+      if(event.target !== event.currentTarget) return
+      $('.lightbox').removeClass("active");
+      $('.lightbox').empty();
+      $("html").removeClass("noscroll");
+   });
+
+});
+
+   var slider_img = document.querySelector('.slider-img');
+   var images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+   var i = 0;
+
+   function prev(){
+      if(i <= 0) i = images.length;	
+      i--;
+      return setImg();			 
+   }
+
+   function next(){
+      if(i >= images.length-1) i = -1;
+      i++;
+      return setImg();			 
+   }
+
+   function setImg(){
+      return slider_img.setAttribute('src', "img/slider/"+images[i]);
+      
+   }
+
+   
+
